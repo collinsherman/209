26e7bcf5c22497bb7397324142cd63c51d4083a0
Collin Sherman <shermanc19@mail.wlu.edu>
2017-09-29 22:34:22 -0400

    Set up FileListing.java file.

---
+++ project3/FileListing.java
@@ -0,0 +1,13 @@
+/*
+Author: Collin Sherman
+File: FileListing.java
+Project: 3
+Outputs text in a file with numbered lines.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class FileListing{
+
+    public static void main(String[] args){
